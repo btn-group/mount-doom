285e099d98a9c2083123476ff9ba6e82ac6044d9
Steven Chang <stevenchang5000@gmail.com>
2021-08-22 13:52:30 +1000

    Query Config

--- src/msg.rs
+++ src/msg.rs
@@ -13,5 +13,11 @@ pub enum HandleMsg {}
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
+    Config {},
+}
+
+// QUERY RESPONSE STRUCTS
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ConfigResponse {
+    pub viewing_key: String,
 }
