09f7b9efd5f09b955e5e4bcf61f4d76eece3efcd
Steven Chang <steven.chang@btn.group>
2021-08-22 16:08:02 +1000

    Enable setting viewing key for snip-20 (#7)

--- examples/schema.rs
+++ examples/schema.rs
@@ -1,6 +1,5 @@
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-use mount_doom::msg::{ConfigResponse, HandleMsg, InitMsg, QueryMsg};
-use mount_doom::state::State;
+use mount_doom::msg::{HandleAnswer, HandleMsg, InitMsg, QueryAnswer, QueryMsg};
 use std::env::current_dir;
 use std::fs::create_dir_all;
 
@@ -11,8 +10,8 @@ fn main() {
     remove_schemas(&out_dir).unwrap();
 
     export_schema(&schema_for!(InitMsg), &out_dir);
+    export_schema(&schema_for!(HandleAnswer), &out_dir);
     export_schema(&schema_for!(HandleMsg), &out_dir);
+    export_schema(&schema_for!(QueryAnswer), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(State), &out_dir);
-    export_schema(&schema_for!(ConfigResponse), &out_dir);
 }
--- src/contract.rs
+++ src/contract.rs
@@ -1,14 +1,15 @@
+use crate::msg::ResponseStatus::Success;
+use crate::msg::{HandleAnswer, HandleMsg, InitMsg, QueryAnswer, QueryMsg};
+use crate::state::{config, config_read, State};
 use cosmwasm_std::{
-    debug_print, to_binary, Api, Binary, Env, Extern, HandleResponse, InitResponse, Querier,
+    to_binary, Api, Binary, Env, Extern, HandleResponse, HumanAddr, InitResponse, Querier,
     StdResult, Storage,
 };
-
-use crate::msg::{ConfigResponse, HandleMsg, InitMsg, QueryMsg};
-use crate::state::{config, config_read, State};
+use secret_toolkit::snip20;
 
 pub fn init<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
-    env: Env,
+    _env: Env,
     msg: InitMsg,
 ) -> StdResult<InitResponse> {
     let state = State {
@@ -25,7 +26,12 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
     env: Env,
     msg: HandleMsg,
 ) -> StdResult<HandleResponse> {
-    match msg {}
+    match msg {
+        HandleMsg::SetViewingKeyForSnip20 {
+            address,
+            contract_hash,
+        } => set_viewing_key_for_snip_20(deps, env, address, contract_hash),
+    }
 }
 
 pub fn query<S: Storage, A: Api, Q: Querier>(
@@ -37,13 +43,34 @@ pub fn query<S: Storage, A: Api, Q: Querier>(
     }
 }
 
-fn query_config<S: Storage, A: Api, Q: Querier>(
-    deps: &Extern<S, A, Q>,
-) -> StdResult<ConfigResponse> {
+fn query_config<S: Storage, A: Api, Q: Querier>(deps: &Extern<S, A, Q>) -> StdResult<QueryAnswer> {
     let state = config_read(&deps.storage).load()?;
 
-    Ok(ConfigResponse {
+    Ok(QueryAnswer::Config {
         viewing_key: state.viewing_key,
+    })
+}
+
+fn set_viewing_key_for_snip_20<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    _env: Env,
+    address: HumanAddr,
+    contract_hash: String,
+) -> StdResult<HandleResponse> {
+    let state = config_read(&deps.storage).load()?;
+
+    Ok(HandleResponse {
+        messages: vec![snip20::set_viewing_key_msg(
+            state.viewing_key,
+            None,
+            1,
+            contract_hash,
+            address,
+        )?],
+        log: vec![],
+        data: Some(to_binary(&HandleAnswer::SetViewingKeyForSnip20 {
+            status: Success,
+        })?),
     })
 }
 
@@ -69,15 +96,41 @@ mod tests {
     #[test]
     fn test_query_config() {
         let (_init_result, deps) = init_helper();
+        let res = from_binary(&query(&deps, QueryMsg::Config {}).unwrap()).unwrap();
+        match res {
+            QueryAnswer::Config { viewing_key } => {
+                assert_eq!(viewing_key, "accepteverythingjusthewayitis".to_string());
+            }
+        }
+    }
 
-        let res = query(&deps, QueryMsg::Config {}).unwrap();
-        let value: ConfigResponse = from_binary(&res).unwrap();
-        let state = config_read(&deps.storage).load().unwrap();
+    #[test]
+    fn test_handle_set_viewing_key_for_snip_20() {
+        let (_init_result, mut deps) = init_helper();
+
+        // = * It calls viewing key for snip 20
+        let handle_msg = HandleMsg::SetViewingKeyForSnip20 {
+            address: HumanAddr::from("token-address"),
+            contract_hash: "token-contract-hash".to_string(),
+        };
+        let handle_result = handle(&mut deps, mock_env("user", &[]), handle_msg);
+        let handle_result_unwrapped = handle_result.unwrap();
         assert_eq!(
-            ConfigResponse {
-                viewing_key: state.viewing_key,
-            },
-            value
+            handle_result_unwrapped.messages,
+            vec![snip20::set_viewing_key_msg(
+                "accepteverythingjusthewayitis".to_string(),
+                None,
+                1,
+                "token-contract-hash".to_string(),
+                HumanAddr::from("token-address"),
+            )
+            .unwrap()],
+        );
+        let handle_result_data: HandleAnswer =
+            from_binary(&handle_result_unwrapped.data.unwrap()).unwrap();
+        assert_eq!(
+            to_binary(&handle_result_data).unwrap(),
+            to_binary(&HandleAnswer::SetViewingKeyForSnip20 { status: Success }).unwrap()
         );
     }
 }
