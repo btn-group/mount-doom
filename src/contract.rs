54e794f371f09d0976c9143b88a9b9cb33db9e8e
Steven Chang <steven.chang@btn.group>
2021-08-22 13:55:26 +1000

    Query Config (#6)

--- src/contract.rs
+++ src/contract.rs
@@ -3,7 +3,7 @@ use cosmwasm_std::{
     StdResult, Storage,
 };
 
-use crate::msg::{CountResponse, HandleMsg, InitMsg, QueryMsg};
+use crate::msg::{ConfigResponse, HandleMsg, InitMsg, QueryMsg};
 use crate::state::{config, config_read, State};
 
 pub fn init<S: Storage, A: Api, Q: Querier>(
@@ -32,5 +32,52 @@ pub fn query<S: Storage, A: Api, Q: Querier>(
     deps: &Extern<S, A, Q>,
     msg: QueryMsg,
 ) -> StdResult<Binary> {
-    match msg {}
+    match msg {
+        QueryMsg::Config {} => to_binary(&query_config(deps)?),
+    }
+}
+
+fn query_config<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+) -> StdResult<ConfigResponse> {
+    let state = config_read(&deps.storage).load()?;
+
+    Ok(ConfigResponse {
+        viewing_key: state.viewing_key,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cosmwasm_std::from_binary;
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, MockApi, MockQuerier, MockStorage};
+
+    // === HELPERS ===
+    fn init_helper() -> (
+        StdResult<InitResponse>,
+        Extern<MockStorage, MockApi, MockQuerier>,
+    ) {
+        let env = mock_env("user", &[]);
+        let mut deps = mock_dependencies(20, &[]);
+        let msg = InitMsg {
+            viewing_key: "accepteverythingjusthewayitis".to_string(),
+        };
+        (init(&mut deps, env.clone(), msg), deps)
+    }
+
+    #[test]
+    fn test_query_config() {
+        let (_init_result, deps) = init_helper();
+
+        let res = query(&deps, QueryMsg::Config {}).unwrap();
+        let value: ConfigResponse = from_binary(&res).unwrap();
+        let state = config_read(&deps.storage).load().unwrap();
+        assert_eq!(
+            ConfigResponse {
+                viewing_key: state.viewing_key,
+            },
+            value
+        );
+    }
 }
