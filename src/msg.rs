78607e1c455097fd8f38d6b448ef5f6d5033d8a5
Steven Chang <stevenchang5000@gmail.com>
2021-08-22 15:59:03 +1000

    Enable setting viewing key for snip-20

--- src/msg.rs
+++ src/msg.rs
@@ -1,3 +1,4 @@
+use cosmwasm_std::HumanAddr;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -6,9 +7,26 @@ pub struct InitMsg {
     pub viewing_key: String,
 }
 
+#[derive(Serialize, Deserialize, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum HandleAnswer {
+    SetViewingKeyForSnip20 { status: ResponseStatus },
+}
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum HandleMsg {}
+pub enum HandleMsg {
+    SetViewingKeyForSnip20 {
+        address: HumanAddr,
+        contract_hash: String,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryAnswer {
+    Config { viewing_key: String },
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
@@ -16,8 +34,8 @@ pub enum QueryMsg {
     Config {},
 }
 
-// QUERY RESPONSE STRUCTS
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct ConfigResponse {
-    pub viewing_key: String,
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum ResponseStatus {
+    Success,
 }
